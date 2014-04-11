08488912ab49005162c9133d0b9dbc4cb1a01fea
blelump <blelump@martex.nom.pl>
2014-04-11 16:37:42 +0200

    Upgrage jquery and add jquery.validate.
    Upgrade jquery to 2.1.0
    
    Change-Id: I670bc014de5a48f0c2f52959bc38e0c8a291738f

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,33 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-1.11.0.js
+//= require jquery-2.1.0.js
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.js
 //= require modernizr.custom.24648.js
 //= require jquery.busybox.js
+//= require jquery.validate.js
 //= require core.engine.js
 //= require main
 
+$.validator.setDefaults({
+    errorElement: "span",
+    errorClass: "help-block text-right",
+    highlight: function (element, errorClass, validClass) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorPlacement: function (error, element) {
+        if (element.parent('.input-group').length
+           || element.prop('type') === 'checkbox'
+           || element.prop('type') === 'radio')
+        {
+           error.insertAfter(element.parent());
+        } else {
+           error.insertAfter(element);
+        }
+    }
+});
