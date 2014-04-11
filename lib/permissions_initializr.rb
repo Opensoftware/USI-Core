b37857bbe28b3b1a7c6429b21db6c2ebdc9add25
blelump <blelump@martex.nom.pl>
2014-04-11 12:35:40 +0200

    Add rake task for permissions.
    Change-Id: I1d44c836afcf3a6b622f93f3f6efa70ec2137ef7

---
+++ lib/permissions_initializr.rb
@@ -0,0 +1,62 @@
+require 'find'
+
+module PermissionsInitializr
+    def self.included(base)
+        base.extend PermissionsInitializr
+    end
+
+    def setup_actions_controllers_db()
+        Rails.application.eager_load!
+        ApplicationController.subclasses.each do |controller|
+            load_permission_from_subclasses(controller)
+        end
+        Permission.where(subject_class:['Dashboard', 'UserSession', 'Base'])
+                  .destroy_all
+    end
+
+    def load_permission_from_subclasses(controller)
+        if controller.subclasses.present?
+            controller.subclasses.each do |subcontroller|
+                load_permission_from_subclasses(subcontroller)
+            end
+        end
+        load_permission_from_controller(controller)
+    end
+
+    def load_permission_from_controller(controller)
+        write_permission(controller, "manage")
+        controller.action_methods.each do |action|
+            cancan_action = eval_cancan_action(action)
+            write_permission(controller, cancan_action)
+        end
+    end
+
+    def eval_cancan_action(action)
+        case action.to_s
+        when "index", "show", "search"
+            return "read"
+        when "create", "new"
+            return "create"
+        when "edit", "update"
+            return "update"
+        when "delete", "destroy"
+            return "delete"
+        end
+    end
+
+    def write_permission(controller, cancan_action)
+        return if cancan_action.nil?
+        unless controller == "all"
+            controller = controller.controller_path.classify
+        end
+        permission  = Permission.where(:subject_class => controller)
+                                .where(:action => cancan_action).first
+        unless permission
+            permission = Permission.new
+            permission.subject_class = controller.to_s
+            permission.action = cancan_action
+            permission.save
+        end
+    end
+
+end
