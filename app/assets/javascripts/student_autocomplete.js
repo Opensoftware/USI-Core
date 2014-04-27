5ac3a6fb870916cdc783be83bad088b1037342e0
blelump <blelump@martex.nom.pl>
2014-04-27 16:20:14 +0200

    Make student autocomplete more generic
    Change-Id: I5a0c9ba5a78ad7b834f5acf85c92938db65c42ad

---
+++ app/assets/javascripts/student_autocomplete.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  var students_provider = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: '/students?q=%QUERY'
+  });
+
+  students_provider.initialize();
+
+  $('.typeahead-student').typeahead(null, {
+    name: 'students',
+    displayKey: 'value',
+    source: students_provider.ttAdapter(),
+    templates: {
+      empty: [
+      '<div class="empty-message">',
+      $.i18n._('notice_students_not_found'),
+      '</div>'
+      ].join('\n'),
+      suggestion: Handlebars.compile([
+        '<p class="student-idx-number">{{index_number}}</p>',
+        '<p class="element-data">{{value}}</p>',
+        '<p class="student-studies">{{studies}}</p>'
+        ].join(''))
+    }
+  }).on('typeahead:selected', function(event, datum) {
+    $(event.target).closest("div").children().last().val(datum.id);
+  });
+  $('.typeahead.input-sm').siblings('input.tt-hint').addClass('hint-small');
+  $('.typeahead.input-lg').siblings('input.tt-hint').addClass('hint-large');
+
+});
