aaccf8fae0df2976d87029d0c3193337f13385a0
blelump <blelump@martex.nom.pl>
2014-05-02 19:24:43 +0200

    Forbid student to add new thesis
    Change-Id: I89396687b2883f8ee41c1d3d9c7d302d81531562

--- app/navigations/top_navigation.rb
+++ app/navigations/top_navigation.rb
@@ -10,23 +10,23 @@ if defined?(current_user)
             navigation.item :nav, I18n.t(:label_list_export_pdf), diamond.theses_path(:format => :pdf)
             navigation.item :nav, I18n.t(:label_list_export_xls), diamond.theses_path(:format => :xlsx)
           end
-          primary.item :nav, I18n.t(:label_thesis_new_plural), diamond.new_thesis_path, :class => "inline-icon inline-icon-red inline-icon-plus"
-        when /new|edit/ then
-          primary.item :nav, I18n.t(:label_move_back), diamond.theses_path, :class => "inline-icon inline-icon-red inline-icon-left-arrow"
-        when /show/ then
-          primary.item :nav, I18n.t(:label_move_back), diamond.theses_path, :class => "inline-icon inline-icon-red inline-icon-left-arrow"
-        end
-      elsif controller.controller_name =~ /dashboard/
-        case controller.action_name
-        when /index/
-          if can?(:manage_own, Diamond::Thesis)
-            primary.item :nav, I18n.t(:label_thesis_browse_yours), diamond.theses_path(:supervisor_id => current_user.verifable_id), :if => lambda { current_user.verifable.having_any_theses? }, :class => "inline-icon inline-icon-red inline-icon-thesis-list"
-            primary.item :nav, I18n.t(:label_thesis_new_plural), diamond.new_thesis_path, :class => "inline-icon inline-icon-red inline-icon-plus"
-          elsif current_user.student?
-            primary.item :nav, I18n.t(:label_thesis_browse_field_of_study), diamond.theses_path(:course_ids => current_user.verifable.course_ids.first), :class => "inline-icon inline-icon-red inline-icon-thesis-list"
+          primary.item :nav, I18n.t(:label_thesis_new_plural), diamond.new_thesis_path, if: lambda { can?(:create, Diamond::Thesis) }, :class => "inline-icon inline-icon-red inline-icon-plus"
+          when /new|edit/ then
+            primary.item :nav, I18n.t(:label_move_back), diamond.theses_path, :class => "inline-icon inline-icon-red inline-icon-left-arrow"
+          when /show/ then
+            primary.item :nav, I18n.t(:label_move_back), diamond.theses_path, :class => "inline-icon inline-icon-red inline-icon-left-arrow"
+          end
+        elsif controller.controller_name =~ /dashboard/
+          case controller.action_name
+          when /index/
+            if can?(:manage_own, Diamond::Thesis)
+              primary.item :nav, I18n.t(:label_thesis_browse_yours), diamond.theses_path(:supervisor_id => current_user.verifable_id), :if => lambda { current_user.verifable.having_any_theses? }, :class => "inline-icon inline-icon-red inline-icon-thesis-list"
+              primary.item :nav, I18n.t(:label_thesis_new_plural), diamond.new_thesis_path, :class => "inline-icon inline-icon-red inline-icon-plus"
+            elsif current_user.student?
+              primary.item :nav, I18n.t(:label_thesis_browse_field_of_study), diamond.theses_path(:course_ids => current_user.verifable.course_ids.first), :class => "inline-icon inline-icon-red inline-icon-thesis-list"
+            end
           end
         end
       end
     end
   end
-end
