2582434db65b06fa306f3945184840b3bfb212ee
blelump <blelump@martex.nom.pl>
2014-05-07 19:44:13 +0200

    Make elective blocks link empty (temporary)
    Change-Id: I4155302649760445c86c17e23a8a0ca19ed439a4

--- app/navigations/common_navigation.rb
+++ app/navigations/common_navigation.rb
@@ -2,7 +2,7 @@ if defined?(current_user)
   SimpleNavigation::Configuration.run do |navigation|
     navigation.items do |primary|
       primary.dom_class = 'nav navbar-nav'
-      primary.item :nav, "<i class='icon icon-white icon-elective-blocks'></i> #{t(:label_elective_block_plural)}", 'ok'
+      primary.item :nav, "<i class='icon icon-white icon-elective-blocks'></i> #{t(:label_elective_block_plural)}", ''
       primary.item :nav, "<i class='icon icon-white icon-thesis-list'></i> #{t(:label_thesis_plural_official)}", diamond.theses_path
       if current_user
         primary.item :user_account, "<i class='icon icon-white icon-user'></i> #{current_user.verifable.surname_name}", main_app.user_path(current_user) do |primary|
