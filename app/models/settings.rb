40d365ba7832a96118fa722e611ad876acbc7e63
blelump <blelump@martex.nom.pl>
2014-05-09 14:22:15 +0200

    Add settings.yml to Settings model
    Change-Id: I516f0944fba4a2ed5f77b242f8b50409849bcabe

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -3,4 +3,18 @@ class Settings < ActiveRecord::Base
   belongs_to :annual, :foreign_key => :current_annual_id
   belongs_to :enrollment_semester, :foreign_key => :current_semester_id
 
+  def self.available_settings
+    return @available_settings if defined?(@available_settings)
+    begin
+      @available_settings = YAML::load(File.open("#{Rails.root}/config/settings.yml"))
+    rescue Errno::ENOENT => e
+      @available_settings = {}
+    end
+  end
+
+  self.available_settings.each do |key, v|
+    define_singleton_method key do
+      v
+    end
+  end
 end
