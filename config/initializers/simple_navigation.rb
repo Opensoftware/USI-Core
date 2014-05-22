c0913f79c3d6b3c11af4789946c8b1a3e38433e4
Robert Mitwicki <robert.mitwicki@opensoftware.pl>
2014-05-22 22:36:37 +0200

    Prevent config_file_paths for simple navigation to be override
    Change-Id: Ib859e6a3a79d5c5212414173e52f07758f8573ca

--- config/initializers/simple_navigation.rb
+++ config/initializers/simple_navigation.rb
@@ -1,5 +1,5 @@
-paths = [File.join(Rails.root, 'app', 'navigations')]
-SimpleNavigation.config_file_paths = paths
+paths = File.join(Rails.root, 'app', 'navigations')
+SimpleNavigation.config_file_paths << paths
 #SimpleNavigation.register_renderer :listwithspan => ListWithSpan
 #SimpleNavigation.register_renderer :image => TooltipImagesMenu
 #SimpleNavigation.register_renderer :custom_breadcrumbs => CustomBreadcrumbs
