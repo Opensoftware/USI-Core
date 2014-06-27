5cbf09eb1885295df55f54df66b7d1a08684fcc0
blelump <blelump@martex.nom.pl>
2014-06-27 22:59:12 +0200

    Move enrollment_info method into separate module
    Change-Id: I74102b677b097a105fb472c6f2c1aea35ed06ee0

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -9,21 +9,8 @@ module DashboardHelper
   end
 
   if defined?(Graphite)
-    def elective_block_enrollment_status(elective_mod)
-      if current_user.student.has_pending_enrollments_for_module?(elective_mod)
-        content_tag :span do
-          I18n.t(:label_elective_block_enrollment_pending)
-        end
-      elsif elective_mod.student_enrolled?(current_user.verifable)
-        content_tag :span, :class => "text-success" do
-          I18n.t(:label_elective_block_enrollment_accepted)
-        end
-      else
-        content_tag :span, :class => "text-danger" do
-          I18n.t(:label_elective_block_student_enroll)
-        end
-      end
-    end
+    require 'graphite/enrollment/status_info'
+    include Graphite::Enrollment::StatusInfo
   end
 
   CONTEXT_FILTERS = [:newest, :recently_accepted, :recently_updated, :recently_created, :unaccepted, :newest_enrollments].freeze
