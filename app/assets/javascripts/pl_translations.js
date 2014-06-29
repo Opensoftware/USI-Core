6cadf1c899e571a08e5f013957a7298b0acf2aa3
blelump <blelump@martex.nom.pl>
2014-06-29 16:09:49 +0200

    Add translated message for form validator
    Change-Id: I635783edb46c79b621c01a7fb2e6269d6b28fc4e

--- app/assets/javascripts/pl_translations.js
+++ app/assets/javascripts/pl_translations.js
@@ -12,5 +12,6 @@ var pl_translations = {
   "notice_students_not_found": "W bazie danych nie ma takiego studenta lub został już przypisany do tematu pracy",
   "notice_supervisor_not_found": "Wygląda na to, że w bazie danych nie ma takiego promotora",
   "confirmation_elective_block_delete": "Czy na pewno chcesz usunąć ten moduł obieralny?",
-  "confirmation_elective_blocks_delete": "Czy na pewno chcesz usunąć wybrane moduły obieralne?"
+  "confirmation_elective_blocks_delete": "Czy na pewno chcesz usunąć wybrane moduły obieralne?",
+  "validator_match_subject_amount": "Wybierz dokładnie {0} przedmioty"
 }
