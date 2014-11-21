d90de80b66cc36f366410efd1d1e4122a36de77d
blelump <blelump@martex.nom.pl>
2014-11-21 15:03:38 +0100

    Add form params to PDF 'link-export' link
    Change-Id: I4088f40903b42b7bd0639d75e38889bfa99645cb

--- app/assets/javascripts/filter_form.js
+++ app/assets/javascripts/filter_form.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
     $(window).listHistory();
-
+    $(".link-export").click(function() {
+        $(this).attr("href", $.clear_query_params($(this).attr("href")) + window.location.search);
+    });
     $("select, input[type='radio']", ".filter-form").loader({
         trigger_on_event: "change"
     });
     $("button[type='submit']", ".filter-form").loader();
     $("button.refresh-btn", ".filter-form").cleaner();
-    $('div.content')
-    .per_page_paginator({bindMethod: 'live', selector: 'div.content-footnote .per-page a'})
-    .page_paginator({bindMethod: 'live', selector: 'div.content-footnote .pagination a'});
+    $('div.content').per_page_paginator({
+        bindMethod: 'live',
+        selector: 'div.content-footnote .per-page a'
+    }).page_paginator({
+        bindMethod: 'live',
+        selector: 'div.content-footnote .pagination a'
+    });
 });
