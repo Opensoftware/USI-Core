40e067eb7a892836d8e4e5e0207dc809ec708722
blelump <blelump@martex.nom.pl>
2015-01-21 19:30:33 +0100

    Reset id field of found user unless it exactly matches found user
    Change-Id: Ie85dd171bd1c63244917bcc18c84a2af1a57b3a4

--- app/assets/javascripts/student_autocomplete.js
+++ app/assets/javascripts/student_autocomplete.js
@@ -4,29 +4,27 @@ $(document).ready(function() {
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     remote: '/students?q=%QUERY'
   });
-
   students_provider.initialize();
-
   $('.typeahead-student').typeahead(null, {
     name: 'students',
     displayKey: 'value',
     source: students_provider.ttAdapter(),
     templates: {
-      empty: [
-      '<div class="empty-message">',
-      $.i18n._('notice_students_not_found'),
-      '</div>'
+      empty: ['<div class="empty-message">',
+        $.i18n._('notice_students_not_found'), '</div>'
       ].join('\n'),
-      suggestion: Handlebars.compile([
-        '<p class="student-idx-number">{{index_number}}</p>',
-        '<p class="element-data">{{value}}</p>',
-        '<p class="student-studies">{{studies}}</p>'
-        ].join(''))
+      suggestion: Handlebars.compile(['<p class="student-idx-number">{{index_number}}</p>', '<p class="element-data">{{value}}</p>', '<p class="student-studies">{{studies}}</p>'].join(''))
     }
-  }).on('typeahead:selected', function(event, datum) {
-    $(event.target).closest("div").children().last().val(datum.id);
+  }).on('typeahead:selected typeahead:autocompleted', function(event, datum) {
+    set_student_input($(event.target), datum.id);
+  }).on('keyup', function(event) {
+    if ($('.tt-suggestion').length === 0) {
+      set_student_input($(event.target), "");
+    }
   });
   $('.typeahead.input-sm').siblings('input.tt-hint').addClass('hint-small');
   $('.typeahead.input-lg').siblings('input.tt-hint').addClass('hint-large');
-
 });
+var set_student_input = function(context, value) {
+  context.closest("div").find(".twitter-typeahead").next().val(value);
+};
--- app/assets/javascripts/supervisor_autocomplete.js
+++ app/assets/javascripts/supervisor_autocomplete.js
@@ -4,31 +4,30 @@ var supervisor_autocomplete = function() {
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     remote: '/employees?q=%QUERY'
   });
-
   supervisor_provider.initialize();
-
   $('.typeahead-supervisor').typeahead(null, {
     name: 'supervisor',
     displayKey: 'value',
     source: supervisor_provider.ttAdapter(),
     templates: {
-      empty: [
-      '<div class="empty-message">',
-      $.i18n._('notice_supervisor_not_found'),
-      '</div>'
+      empty: ['<div class="empty-message">',
+        $.i18n._('notice_supervisor_not_found'), '</div>'
       ].join('\n'),
-      suggestion: Handlebars.compile([
-        '<p class="element-data">{{value}}</p>',
-        '<p class="academy-unit">{{academy_unit}}</p>'
-        ].join(''))
+      suggestion: Handlebars.compile(['<p class="element-data">{{value}}</p>', '<p class="academy-unit">{{academy_unit}}</p>'].join(''))
     }
-  }).on('typeahead:selected', function(event, datum) {
-    $(event.target).closest("div").children().last().val(datum.id);
+  }).on('typeahead:selected typeahead:autocompleted', function(event, datum) {
+    set_supervisor_input($(event.target), datum.id);
+  }).on('keyup', function(event) {
+    if ($('.tt-suggestion').length === 0) {
+      set_supervisor_input($(event.target), "");
+    }
   });
   $('.typeahead.input-sm').siblings('input.tt-hint').addClass('hint-small');
   $('.typeahead.input-lg').siblings('input.tt-hint').addClass('hint-large');
 };
-
+var set_supervisor_input = function(context, value) {
+  context.closest("div").find(".twitter-typeahead").next().val(value);
+};
 $(document).ready(function() {
   supervisor_autocomplete();
 });
